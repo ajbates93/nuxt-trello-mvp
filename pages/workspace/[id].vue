5529eb384152e89bd4a2f337604a07a3422f3d43
Alex Bates <ajbates93@gmail.com>
2022-05-06 09:28:04 +0100

    feature: ability to add workspace pages

--- pages/workspace/[id].vue
+++ pages/workspace/[id].vue
@@ -1,9 +1,25 @@
-<script setup>
+<script>
 import { workspaceList } from '../../store/global.js'
+export default {
+  setup() {
+    return {
+      workspaceList
+    }
+  },
+  data: () => ({
+    workspaceName: ''
+  }),
+  mounted() {
+    this.workspaceName = this.workspaceList.find(
+      workspace => workspace.id === Number(this.$route.params.id
+    )
+    ).name
+  }
+}
 </script>
 
 <template>
   <div>
-    <h1>Workspace {{ $route.params.id }}</h1>
+    <h1>{{ workspaceName }} Workspace (#{{ $route.params.id }})</h1>
   </div>
 </template>
